82ec1d572d1af07c872bfd4b258e4963e2ad287c
himawansutanto <zawaoga@gmail.com>
2022-05-30 13:21:43 +0700

    tugas 3

---
+++ ch3.js
@@ -0,0 +1,22 @@
+function romawi(n){
+    let desimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let romawiHuruf = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    let hasil ='';
+
+    for (let i = 0; i < romawiHuruf.length; i++) {
+        while(desimal[i] <= n){
+            hasil += romawiHuruf[i];
+            n -= desimal[i];
+        }
+    }
+    return hasil;
+}
+
+console.log("Script Testing untuk Konversi Romawi\n");
+console.log("Input | expected | result");
+console.log("------|----------|-------");
+console.log("4     | IV       | ", romawi(4));
+console.log("9     | IX       | ", romawi(9));
+console.log("13    | XIII     | ", romawi(13));
+console.log("1453  | MCDLIII  | ", romawi(1453));
+console.log("1646  | MDCXLVI  | ", romawi(1646));

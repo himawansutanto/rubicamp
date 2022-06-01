31f456fe21ca1dd3f3b6eae07e16aa4ad8106bf7
himawansutanto <zawaoga@gmail.com>
2022-06-01 08:23:58 +0700

    tugas 3

--- ch3.js
+++ ch3.js
@@ -1,12 +1,12 @@
-function romawi(n){
+function romawi(n) {
     let desimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     let romawiHuruf = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-    let hasil ='';
+    let hasil = '';
 
-    for (let i = 0; i < romawiHuruf.length; i++) {
-        while(desimal[i] <= n){
-            hasil += romawiHuruf[i];
-            n -= desimal[i];
+    for (let index = 0; index < romawiHuruf.length; index++) {
+        while (desimal[index] <= n) {
+            hasil += romawiHuruf[index];
+            n -= desimal[index];
         }
     }
     return hasil;

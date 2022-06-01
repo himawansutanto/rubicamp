d3fe921b2fbb37bcf3678c30b91a601f16cdffbc
himawansutanto <zawaoga@gmail.com>
2022-06-01 08:20:02 +0700

    tugas 1

--- ch1.js
+++ ch1.js
@@ -1,11 +1,11 @@
-function sum(a,b,c){
+function sum() {
     let number = 0;
-        for(let i = 0; i < arguments.length; i++) {
-            number = number + arguments[i];
-        }
-        return number;
+    for (let index = 0; index < arguments.length; index++) {
+        number = number + arguments[index];
     }
-console.log(sum(1,2,7));
-console.log(sum(1,4));
-console.log(sum(11));
-console.log(sum(10,3,6,7,9));
+    console.log(number);
+}
+sum(1, 2, 7);
+sum(1, 4);
+sum(11);
+sum(10, 3, 6, 7, 9);

bbcf18ebece04a4dcf43fe398c90e501f020650c
himawansutanto <zawaoga@gmail.com>
2022-06-04 11:10:55 +0700

    tugas 8

---
+++ ch8.js
@@ -0,0 +1,18 @@
+function pola(str) {
+    let number = str.split('=');
+    let number1 = number[0].split('*')[0].trim();
+    let number2 = number[0].split('*')[1].trim();
+    let number3 = number[1].trim();
+
+    console.log(number1, number2, number3);
+    for (let i = 0; i <= 9; i++) {
+        for (let j = 0; j <= 9; j++) {
+            if (parseInt(number1.replace('#', i)) * parseInt(number2) == parseInt(number3.replace('#', j)))
+                return [i, j];
+        }
+    }
+}
+console.log(pola("42#3 * 188 = 80#204"));
+// result [8, 5]
+console.log(pola("8#61 * 895 = 78410#5"));
+// result [7,9]

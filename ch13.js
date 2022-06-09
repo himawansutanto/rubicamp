fbc7d2b3bc1062e9270a660ee7e1ea9b9e3368c6
himawansutanto <zawaoga@gmail.com>
2022-06-09 21:35:34 +0700

    tugas13

---
+++ ch13.js
@@ -0,0 +1,9 @@
+const readLine = require('readLine');
+const fs = require('fs');
+
+let data = JSON.parse(fs.readFileSync("data.json", "utf8"));
+let input = process.argv
+
+switch (process.argv[2]) {
+    
+}

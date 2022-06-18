35f5774e2ce5cb219670e0547e522a5c0e95e2d1
himawansutanto <zawaoga@gmail.com>
2022-06-18 23:53:03 +0700

    done

--- ch14/ch14.sql
+++ ch14/ch14.sql
@@ -24,6 +24,7 @@ CREATE TABLE Matakuliah (
 
 CREATE TABLE Kontrak (
     idkontrak       INTEGER PRIMARY KEY AUTOINCREMENT,
+    nama            VARCHAR(100) NOT NULL,
     nilai           VARCHAR(5),
     nipdosen        VARCHAR(5) NOT NULL,
     kodematakuliah  VARCHAR(5) NOT NULL,
@@ -57,10 +58,39 @@ INSERT INTO Matakuliah VALUES ('B1011', 'Bahasa Arab', 2 );
 INSERT INTO Matakuliah VALUES ('A1012', 'Anatomi', 3 );
 INSERT INTO Matakuliah VALUES ('M1013', 'Matematika IA', 3 );
 INSERT INTO Matakuliah VALUES ('A1014', 'Astrodinamika', 3 );
-INSERT INTO Matakuliah VALUES ('P1015', 'Data Mining', 3);
+INSERT INTO Matakuliah VALUES ('P1015', 'Data Mining', 3 );
+INSERT INTO Matakuliah VALUES ('P1016', 'Struktur Data', 3 );
+INSERT INTO Matakuliah VALUES ('P1017', 'Algoritma dan Pemrograman', 3 );
+INSERT INTO Matakuliah VALUES ('P1018', 'Jaringan Komputer', 3 );
 
-INSERT INTO Kontrak (nilai, nipdosen, kodematakuliah, nim) VALUES ( 'A', 101090, 'B1011', 10109250);
-INSERT INTO Kontrak (nilai, nipdosen, kodematakuliah, nim) VALUES ( 'B', 101091, 'A1012', 10109251);
-INSERT INTO Kontrak (nilai, nipdosen, kodematakuliah, nim) VALUES ( 'B', 101092, 'M1013', 10109252);
-INSERT INTO Kontrak (nilai, nipdosen, kodematakuliah, nim) VALUES ( 'D', 101093, 'A1014', 10109253);
-INSERT INTO Kontrak (nilai, nipdosen, kodematakuliah, nim) VALUES ( 'E', 101094, 'P1015', 10109254);
+INSERT INTO Kontrak (nama, nilai, nipdosen, kodematakuliah, nim) VALUES ('Wildan', 'A', 101090, 'B1011', 10109250);
+INSERT INTO Kontrak (nama, nilai, nipdosen, kodematakuliah, nim) VALUES ('Hilmi', 'B', 101091, 'A1012', 10109251);
+INSERT INTO Kontrak (nama, nilai, nipdosen, kodematakuliah, nim) VALUES ('Nanda', 'B', 101092, 'M1013', 10109252);
+INSERT INTO Kontrak (nama, nilai, nipdosen, kodematakuliah, nim) VALUES ('Rahmat', 'D', 101093, 'A1014', 10109253);
+INSERT INTO Kontrak (nama, nilai, nipdosen, kodematakuliah, nim) VALUES ('Alam', 'A', 101090, 'A1012', 10109254);
+INSERT INTO Kontrak (nama, nilai, nipdosen, kodematakuliah, nim) VALUES ('Rizky', 'A', 101094, 'B1011', 10109255);
+INSERT INTO Kontrak (nama, nilai, nipdosen, kodematakuliah, nim) VALUES ('Himawan', 'D', 101094, 'P1015', 10109256);
+INSERT INTO Kontrak (nama, nilai, nipdosen, kodematakuliah, nim) VALUES ('Himawan', 'D', 101094, 'P1016', 10109256);
+INSERT INTO Kontrak (nama, nilai, nipdosen, kodematakuliah, nim) VALUES ('Himawan', 'D', 101094, 'P1017', 10109256);
+INSERT INTO Kontrak (nama, nilai, nipdosen, kodematakuliah, nim) VALUES ('Himawan', 'E', 101094, 'P1018', 10109256);
+
+
+SELECT Mahasiswa.nama, Mahasiswa.kodejurusan FROM Mahasiswa;
+SELECT Mahasiswa.nama, Mahasiswa.umur FROM Mahasiswa WHERE umur <20;
+SELECT Mahasiswa.nama, Mahasiswa.nim, Kontrak.nilai FROM Mahasiswa, Kontrak WHERE Mahasiswa.nim = Kontrak.nim AND Kontrak.nilai <"B";
+SELECT Mahasiswa.nama, Mahasiswa.nim, Matakuliah.kodematakuliah, Matakuliah.nama FROM Mahasiswa, Matakuliah, Kontrak WHERE Kontrak.nim = Mahasiswa.nim AND Kontrak.kodematakuliah = Matakuliah.kodematakuliah AND Matakuliah.nama = "Data Mining";
+SELECT Mahasiswa.nama, Mahasiswa.nim, sum(Matakuliah.sks) FROM Mahasiswa, Kontrak, Matakuliah WHERE Mahasiswa.nim = Kontrak.nim AND Kontrak.kodematakuliah = Matakuliah.kodematakuliah GROUP BY Mahasiswa.nim HAVING sum(Matakuliah.sks) >10;
+SELECT dosen.nipdosen, dosen.nama, COUNT(Mahasiswa.nim) FROM Mahasiswa, dosen, Kontrak WHERE dosen.nipdosen = Kontrak.nipdosen AND Mahasiswa.nim = Kontrak.nim AND Kontrak.nama = Mahasiswa.nama GROUP BY dosen.nama;
+SELECT Mahasiswa.nama, Mahasiswa.nim, Mahasiswa.umur FROM Mahasiswa ORDER BY umur ASC;
+SELECT Mahasiswa.nama, Mahasiswa.nim, Mahasiswa.umur FROM Mahasiswa ORDER BY umur DESC;
+SELECT Mahasiswa.nim, Mahasiswa.nama, Jurusan.namajurusan, Dosen.nama, Matakuliah.nama, Kontrak.nilai FROM Mahasiswa JOIN  Jurusan JOIN dosen JOIN  Matakuliah JOIN  Kontrak WHERE Mahasiswa.nim = Kontrak.nim AND Matakuliah.kodematakuliah = Kontrak.kodematakuliah AND dosen.nipdosen = Kontrak.nipdosen AND Mahasiswa.kodejurusan = Jurusan.namajurusan AND Kontrak.nilai >"C";
+
+ALTER TABLE Mahasiswa ADD COLUMN umur VARCHAR(2);
+
+UPDATE Mahasiswa SET umur = 22 WHERE nim =10109250;
+UPDATE Mahasiswa SET umur = 22 WHERE nim =10109251;
+UPDATE Mahasiswa SET umur = 23 WHERE nim =10109252;
+UPDATE Mahasiswa SET umur = 21 WHERE nim =10109253;
+UPDATE Mahasiswa SET umur = 18 WHERE nim =10109254;
+UPDATE Mahasiswa SET umur = 19 WHERE nim =10109255;
+UPDATE Mahasiswa SET umur = 25 WHERE nim =10109256;
